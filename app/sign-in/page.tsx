1cc96a1ec4c738af860fbe3f3830ead5e1bfdc81
venu-chandaka <venuchandaka513@gmail.com>
2026-02-04 21:57:05 +0530

    developed basic ui for the project

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import AuthForm from '@/components/Authform'
+import React from 'react'
+
+const sign = () => {
+  return <AuthForm type="sign-in" />
+}
+
+export default sign
---
+++ app/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import AuthForm from '@/components/Authform'
+
+const page = () => {
+  return <AuthForm type="sign-up" />
+}
+
+export default page
