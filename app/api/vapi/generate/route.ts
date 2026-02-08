7a84c7553addbf34e544aa6a5a3168a7355cdbe5
venu-chandaka <venuchandaka513@gmail.com>
2026-02-08 12:43:41 +0530

    implemeneted interview generation

---
+++ app/api/vapi/generate/route.ts
@@ -0,0 +1,55 @@
+import { generateText } from "ai";
+import { google } from "@ai-sdk/google";
+
+import { success } from "zod";
+
+import { db } from "@/firebase/admin";
+import { getRandomInterviewCover } from "@/lib/utils";
+
+
+export async function POST(request: Request) {
+  const { type, role, level, techstack, amount, userid } = await request.json();
+
+  try {
+    const { text: questions } = await generateText({
+      model: google("gemini-2.5-flash"),
+      prompt: `Prepare questions for a job interview.
+        The job role is ${role}.
+        The job experience level is ${level}.
+        The tech stack used in the job is: ${techstack}.
+        The focus between behavioural and technical questions should lean towards: ${type}.
+        The amount of questions required is: ${amount}.
+        Please return only the questions, without any additional text.
+        The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
+        Return the questions formatted like this:
+        ["Question 1", "Question 2", "Question 3"]
+        
+        Thank you! <3
+    `,
+    });
+
+    // Construct the object carefully
+    const interviewData = {
+      role: String(role),
+      type: String(type), // Force it to be a string
+      level: String(level),
+      techstack: String(techstack).split(",").map(s => s.trim()),
+      questions: JSON.parse(questions || "[]"),
+      userId: String(userid),
+      finalized: true,
+      coverImage: getRandomInterviewCover(),
+      createdAt: new Date().toISOString(),
+    };
+
+    await db.collection("interviews").add(interviewData);
+
+    return Response.json({ success: true }, { status: 200 });
+  } catch (error) {
+    console.error("Error:", error);
+    return Response.json({ success: false, error: error }, { status: 500 });
+  }
+}
+
+export async function GET() {
+  return Response.json({ success: true, data: "Thank you!" }, { status: 200 });
+}
